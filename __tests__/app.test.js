cf4f174391af7a3a04be4055aa1356440f6a09a9
yos244 <yos_24@hotmail.com>
2022-11-16 08:50:27 +0000

    finished ticket 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,19 +3,17 @@ const app = require("../app.js");
 const seed = require("../db/seeds/seed.js");
 const testData = require("../db/data/test-data");
 const db = require("../db/connection.js");
-const { response } = require("../app.js");
-const { string } = require("pg-format");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
-describe("3 api/categories", () => {
+describe(" api/categories", () => {
   test("GET:200 recieves an array with api/categories", () => {
     return request(app)
       .get("/api/categories")
       .expect(200)
       .then((response) => {
-        // (_body error)           {body: {categories}} 
+        // (_body error)           {body: {categories}}
         expect(response.body).toHaveLength(4);
         response.body.forEach((category) => {
           expect(category).toMatchObject({
@@ -25,16 +23,42 @@ describe("3 api/categories", () => {
         });
       });
   });
-  
-});
-
-describe('Error handling', () => {
-  test('GET 404 not found', () => {
+  test("GET 200 - Returns an array of review objects with '`owner` which is the `username` from the users table`title`,`review_id`, `category`,`review_img_url`,`created_at`,`votes` ,`designer`,`comment_count` which is the total count of all the comments with this review_id", () => {
     return request(app)
-    .get("/api/categ")
-    .expect(404)
-    .then((response)=>{
-      expect(response.body.msg).toBe(`Not found`)
-    })
+      .get("/api/reviews")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).not.toHaveLength(0);
+        response.body.forEach((review) => {
+          let dateVal = 0;
+          expect(review).toMatchObject({
+            owner: expect.any(String),
+            title: expect.any(String),
+            review_id: expect.any(Number),
+            category: expect.any(String),
+            review_img_url: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            designer: expect.any(String),
+            comment_count: expect.any(Number),
+          });
+          const checkDate = Date.parse(review.created_at) > dateVal;
+          dateVal = Date.parse(review.created_at);
+
+          expect(checkDate).toBe(true);
+        });
+      });
   });
 });
+
+describe("Error handling", () => {
+  test("GET 404 not found", () => {
+    return request(app)
+      .get("/api/categ")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe(`Not found`);
+      });
+});
+})
+
--- model/model.js
+++ model/model.js
@@ -1,12 +1,33 @@
-
-const db = require('../db/connection.js');
+const { response } = require("../app.js");
+const db = require("../db/connection.js");
 
 exports.selectCategories = () => {
-    return db.query (`
+  return db
+    .query(
+      `
     SELECT * FROM categories;
-    `)
-    .then((response) =>{
-        return response.rows
-    })
-}
+    `
+    )
+    .then((response) => {
+      return response.rows;
+    });
+};
 
+exports.selectReviews = () => {
+  return db
+    .query(
+      `
+      SELECT reviews.owner, reviews.title, reviews.review_id, reviews.category, reviews.review_img_url, reviews.created_at, reviews.votes, reviews.designer,  COUNT(comment_id) AS comment_count
+      FROM reviews
+      LEFT JOIN comments ON comments.review_id = reviews.review_id
+      GROUP BY reviews.review_id
+      ORDER BY reviews.created_at DESC;
+      `
+    )
+    .then((response) => {
+      response.rows.forEach((review) => {
+        review.comment_count = Number(review.comment_count);
+      });
+      return response.rows;
+    });
+};
