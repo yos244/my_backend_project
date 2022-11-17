203112a5e1d43ddd730e338cd610544dd3d42a78
yos244 <yos_24@hotmail.com>
2022-11-17 14:29:57 +0000

    pushing ticket 8

--- controller/controller.js
+++ controller/controller.js
@@ -6,6 +6,7 @@ const {
   selectReviewWithId,
   selectComments,
   insertComment,
+  editVotes,
 } = require("../model/model.js");
 
 exports.getCategories = (req, res, next) => {
@@ -31,17 +32,29 @@ exports.getReviewsWithId = (req, res, next) => {
 };
 
 exports.getComments = (req, res, next) => {
-  selectComments(req.params.review_id).then((comments) => {
-    res.status(200).send(comments);
-  }).catch((err)=>{
-    next(err)
-  })
+  selectComments(req.params.review_id)
+    .then((comments) => {
+      res.status(200).send(comments);
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
-exports.postComment = (req,res,next) => {
-    insertComment(req.params.review_id,req.body).then((comment)=>{
-      res.status(201).send(comment)
-    }).catch((err)=>{
-      next(err)
+exports.postComment = (req, res, next) => {
+  insertComment(req.params.review_id, req.body)
+    .then((comment) => {
+      res.status(201).send(comment);
     })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.patchReview = (req, res, next) => {
+  editVotes(req.params, req.body.inc_votes).then((review) => {
+    res.status(200).send(review);
+  }).catch((err)=>{
+    next(err)
+  });
+};
