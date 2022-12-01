4a7c7eb401b155121f480620e60d8979d0bc3700
yos244 <yos_24@hotmail.com>
2022-12-01 12:08:13 +0000

    123

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,15 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
 }
 
-module.exports = new Pool();
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+      }
+    : {};
+
+module.exports = new Pool(config);
